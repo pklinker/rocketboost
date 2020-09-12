d350efbe5efc68d9899f102050741ed7f3fd8f1b
pklinker <pklinker@gmail.com>
2020-09-12 09:02:49 -0400

    Added rocket control - sound, thrust, and steering.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource engineAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        engineAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            // turn off engine noise when thrusters stop
+            engineAudioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Rotating Left
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            // Rotating Right
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    /**
+     * Apply thrust and play effects/sounds.
+     */
+    private void ApplyThrust()
+    {
+        Vector3 force = Vector3.up;
+        // add thrust
+        rigidBody.AddRelativeForce(force);
+        if (!engineAudioSource.isPlaying)
+            engineAudioSource.Play();
+    }
+}
